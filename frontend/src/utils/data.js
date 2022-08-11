9f5a87ba1f20ee1912845086e8ba13a5235aa778
SorinC6 <kish.sorin@yahoo.com>
2022-08-11 16:28:04 +0300

    added query for search

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -4,6 +4,31 @@ export const userQuery = (userId) => {
   return query;
 };
 
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match]{
+    image{
+      asset -> {url}
+    },
+    _id,
+    destination,
+    postedBy -> {
+      _id,
+      username,
+      image
+    },
+    save[]{
+      _key,
+      postedBy -> {
+        _id,
+        username,
+        image
+      },
+    },
+  }`;
+
+  return query;
+};
+
 export const categories = [
   {
     name: "cars",
